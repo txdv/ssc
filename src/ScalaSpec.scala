835f0eea4b8a7aba2b13f9e7bf0b114793afcd21
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-02-22 13:07:59 +0200

    make tests terser

--- src/ScalaSpec.scala
+++ src/ScalaSpec.scala
@@ -10,77 +10,77 @@ import flatspec._
 import matchers._
 import Expression._
 
+object Helper {
+  implicit class StringOps(val string: String) {
+    def ast[A](parser: Parser[A]): Option[A] = Util.check {
+      Parser.parse(parser, string.stripMargin.getBytes)
+    }
+
+  }
+}
+
 class ScalaSpec extends AnyFlatSpec with should.Matchers {
+  import Helper._
 
   "import" should "parse a simple import" in {
-    Util.check {
-      Parser.parse(Scala.`import`, "import some.other.value123\n".getBytes)
-    } should be (Some(Import("some.other.value123")))
+    "import some.other.value123\n".ast(Scala.`import`) should be (Some(
+      Import("some.other.value123")
+    ))
   }
 
   "import" should "parse an import only if the imported name is on one line" in {
-    Util.check {
-      Parser.parse(Scala.`import`, "import some.other\n.value123".getBytes)
-    } should be (None)
+    "import some.other\n.value123".ast(Scala.`import`) should be (None)
   }
 
   "main" should "parse a singular import" in {
-    Util.check {
-      Parser.parse(Scala.main, "import some.other.value123".getBytes)
-    } should be (Some(List(Import("some.other.value123"))))
+    "import some.other.value123".ast(Scala.main) should be (Some(
+      List(Import("some.other.value123"))
+    ))
   }
 
   "main" should "parse a multiple imports" in {
-    Util.check {
-      Parser.parse(Scala.main, "import some.other.value123\nimport some.other.value2".getBytes)
-    } should be (Some(List(Import("some.other.value123"), Import("some.other.value2"))))
+    val src = "import some.other.value123\nimport some.other.value2"
+    src.ast(Scala.main) should be (Some(
+      List(Import("some.other.value123"), Import("some.other.value2"))
+    ))
   }
 
   "defMethod" should "parse simple method definition" in {
-    Util.check {
-      Parser.parse(Scala.defMethod, "def method_name: Unit = ???".getBytes)
-    } should be (Some(DefMethod("method_name")))
+    "def method_name: Unit = ???".ast(Scala.defMethod) should be (Some(
+      DefMethod("method_name")
+    ))
   }
 
   "expr" should "parse symbol identifier reference" in {
-    Util.check {
-      Parser.parse(Scala.expr.ident, "???".getBytes)
-    } should be (Some(Ident("???")))
-
+    "???".ast(Scala.expr.ident) should be (Some(Ident("???")))
   }
 
   "defMethod" should "parse method with expr surrounded in { }" in {
-    val src = """
+    """
       |def method_name: Unit = {
       |  ???
-      |}""".stripMargin.getBytes
-
-    Util.check {
-      Parser.parse(Scala.defMethod, src)
-    } should be (Some(DefMethod("method_name")))
+      |}""".ast(Scala.defMethod) should be (Some(
+        DefMethod("method_name")
+      ))
   }
 
   "defObject" should "parse empty object definition" in {
-    Util.check {
-      Parser.parse(Scala.defObject, "object empty{}".getBytes)
-    } should be (Some(DefObject("empty")))
+    "object empty{}".ast(Scala.defObject) should be (Some(DefObject("empty")))
   }
 
   "defObject" should "parse multiple statements" in {
-    Util.check {
-      Parser.parse(Scala.main, "import a.b.c\nobject empty{}\n".getBytes)
-    } should be (Some(List(Import("a.b.c"), DefObject("empty"))))
+    "import a.b.c\nobject empty{}\n".ast(Scala.main) should be (Some(
+      List(Import("a.b.c"), DefObject("empty"))
+    ))
   }
 
   "defObject" should "parse an empty method in an object" in {
-    val src = """
+    """
       |object Main {
-      |  def method: Unit = ???
+      |  def methodName: Unit = ???
       |}
-      |""".stripMargin
-
-    Util.check {
-      Parser.parse(Scala.defObject, src.getBytes)
-    } should not be (None)
+      |""".ast(Scala.defObject) should be (Some(
+        DefObject("Main", Seq(DefMethod("methodName")))
+      ))
   }
 }
