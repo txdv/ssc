06deffe5a66564d29a9ff1642fd5cf3ce4fbb61f
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-03-01 11:01:05 +0200

    add class file parsing

---
+++ src/Class.scala
@@ -0,0 +1,177 @@
+package lt.vu.mif.bentkus.bachelor.compiler.classfile.higher
+
+import lt.vu.mif.bentkus.bachelor.compiler.classfile.{MainApp => OldApp, ClassFile, MethodInfo, Span}
+import java.nio.ByteBuffer
+
+sealed trait JavaType
+
+case object JavaInt extends JavaType
+case object JavaChar extends JavaType
+case object JavaVoid extends JavaType
+
+case class JavaClass(namespace: String) extends JavaType {
+  private val tmp = namespace.split("/").reverse
+  val pkg = tmp.drop(1).reverse.mkString("/")
+  val name = tmp.head
+}
+
+object JavaClass {
+  def isChar(ch: Char): Boolean =
+    ch.isLetterOrDigit || ch == '_' || ch == '/'
+}
+
+case class JavaGeneric(namespace: String, genericType: JavaType)
+
+object JavaType {
+  def from(a: String): JavaType = {
+    a(0) match {
+      case 'L' =>
+        JavaClass(a.drop(1).takeWhile(JavaClass.isChar))
+      case '[' =>
+        Arr(from(a.drop(1)))
+      case 'I' =>
+        JavaInt
+      case 'C' =>
+        JavaChar
+      case 'V' =>
+        JavaVoid
+      case _ =>
+        throw new Exception("invalid format")
+    }
+  }
+
+  def method(str: String): Seq[JavaType] = {
+    Seq.empty
+  }
+}
+
+case class Arr(info: JavaType) extends JavaType {
+  val arity: Int = info match {
+    case arr: Arr => 1 + arr.arity
+    case _ => 1 }
+}
+/*
+sealed trait JavaType
+
+case object JavaInt extends JavaType
+case object JavaVoid extends JavaType
+case class JavaClass(name: String)
+*/
+
+case class Method(
+  name: String,
+  taip: String,
+  access: Set[AccessFlag])
+
+case class Class(
+  thisClass: JavaClass,
+  superClass: JavaClass,
+  methods: Seq[Method],
+  attributes: Seq[Attribute])
+
+
+sealed trait Attribute {
+  val name: String
+}
+
+sealed trait KeyValueAttribute extends Attribute {
+  val value: String
+
+}
+case class SignatureAttribute(name: String, value: String) extends KeyValueAttribute
+case class SourceFileAttribute(name: String, value: String) extends KeyValueAttribute
+
+case class GenericAttribute(name: String, value: Array[Byte]) extends Attribute
+
+sealed trait AccessFlag {
+  def value: Int
+
+  def isSet(int: Int): Boolean = {
+    (int & value) == value
+  }
+}
+
+case object Public extends AccessFlag {
+  val value: Int = 0x0001
+}
+
+case object Private extends AccessFlag {
+  def value: Int = 0x0010
+}
+
+case object Super extends AccessFlag {
+  def value: Int = 0x0020
+}
+
+case object Interface extends AccessFlag {
+  def value: Int = 0x0200
+}
+
+case object Abstract extends AccessFlag {
+  def value: Int = 0x0400
+}
+
+case object Annotation extends AccessFlag {
+  def value: Int = 0x2000
+}
+
+case object Enum extends AccessFlag {
+  def value: Int = 0x4000
+}
+
+object AccessFlag {
+  val values = Set[AccessFlag](
+    Public,
+    Private,
+    Super,
+    Interface,
+    Abstract,
+    Annotation,
+    Enum
+  )
+
+  def parse(af: Int): Set[AccessFlag] = {
+    values.filter(_.isSet(af))
+  }
+}
+
+object Converter {
+  def convert(classFile: ClassFile): Class = {
+    val consts = classFile.constants
+
+    val methods = classFile.methods.map { case MethodInfo(accessFlags, name, descriptor, attributes) =>
+      Method(
+        name = classFile.string(name),
+        taip = classFile.string(descriptor),
+        access = AccessFlag.parse(accessFlags))
+    }
+
+    val attributes = classFile.attributes.map { attribute =>
+      val name = classFile.string(attribute.name)
+      val bb = ByteBuffer.wrap(attribute.info)
+      name match {
+        case "Signature" =>
+          SignatureAttribute(name, classFile.string(bb.getShort))
+        case "SourceFile" =>
+          SourceFileAttribute(name, classFile.string(bb.getShort))
+        case _ =>
+          GenericAttribute(name, attribute.info)
+      }
+    }
+
+    println(classFile.fields)
+
+    Class(
+      thisClass = JavaClass(classFile.className(classFile.thisClass)),
+      superClass = JavaClass(classFile.className(classFile.superClass)),
+      methods,
+      attributes)
+  }
+}
+
+object MainApp extends App {
+
+  val classFile = OldApp.read(args.head)
+
+  println(Converter.convert(classFile))
+}
---
+++ src/ClassFile.scala
@@ -0,0 +1,194 @@
+package lt.vu.mif.bentkus.bachelor.compiler.classfile
+
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.file.Files
+
+case class Version(minor: Int, major: Int)
+
+case class ClassFile(
+  version: Version,
+  constants: Seq[Constant],
+  accessFlags: Int,
+  thisClass: Int,
+  superClass: Int,
+  fields: Seq[FieldInfo],
+  methods: Seq[MethodInfo],
+  attributes: Seq[AttributeInfo]) {
+
+  def string(index: Int): String = {
+    constants(index - 1).asInstanceOf[Utf8].string
+  }
+
+  def klass(index: Int): Class = {
+    constants(index -1).asInstanceOf[Class]
+  }
+
+  def className(index: Int): String = {
+    string(klass(index).name)
+  }
+}
+
+sealed trait Constant
+
+case class Class(name: Int) extends Constant
+case class MethodRef(klass: Int, nameType: Int) extends Constant
+case class FieldRef(klass: Int, nameType: Int) extends Constant
+case class Utf8(string: String) extends Constant
+case class NameAndType(index: Int, descriptor: Int) extends Constant
+case class ConstString(index: Int) extends Constant
+
+case class MethodInfo(accessFlags: Int, name: Int, descriptor: Int, attributes: Seq[AttributeInfo])
+case class AttributeInfo(name: Int, info: Array[Byte])
+case class FieldInfo(accessFlags: Int, name: Int, descriptor: Int, attributes: Seq[AttributeInfo])
+
+object ClassFile {
+  def parse(byte: Array[Byte]): ClassFile = {
+    val bb = ByteBuffer.wrap(byte)
+    val magic = bb.getInt
+    val version = Version(bb.getShort, bb.getShort)
+
+    val poolCount = bb.getShort
+
+    val constants = (1 to poolCount - 1).map(_ => getConstant(bb))
+
+    val accessFlags = bb.getShort
+    val thisClass = bb.getShort
+    val superClass = bb.getShort
+
+    val interfacesCount = bb.getShort
+    // TODO: read interfaces
+    val fieldsCount = bb.getShort
+
+    val fields = (1 to fieldsCount).map(_ => readFieldInfo(bb))
+
+    val methodsCount = bb.getShort
+
+    val methods = (1 to methodsCount).map { _ =>
+      readMethodInfo(bb)
+    }
+
+    val attributes = readAttributes(bb)
+
+    ClassFile(version, constants, accessFlags, thisClass, superClass, fields, methods, attributes)
+  }
+
+  private def readMethodInfo(bb: ByteBuffer): MethodInfo = {
+    val accessFlags = bb.getShort
+    val name = bb.getShort
+    val descriptor = bb.getShort
+
+    val attributes = readAttributes(bb)
+
+    MethodInfo(accessFlags, name, descriptor, attributes)
+  }
+
+  private def readFieldInfo(bb: ByteBuffer): FieldInfo = {
+    val accessFlags = bb.getShort
+    val name = bb.getShort
+    val descriptor = bb.getShort
+
+    val attributes = readAttributes(bb)
+
+    FieldInfo(accessFlags, name, descriptor, attributes)
+  }
+
+  def readAttributes(bb: ByteBuffer): Seq[AttributeInfo] = {
+    val attributeCount = bb.getShort
+
+    (1 to attributeCount).map { _ =>
+      readAttribute(bb)
+    }
+  }
+
+  def readAttribute(bb: ByteBuffer): AttributeInfo = {
+    val name = bb.getShort
+    val length = bb.getInt
+    val info = new Array[Byte](length)
+    bb.get(info)
+    AttributeInfo(name, info)
+  }
+
+  def getConstant(bb: ByteBuffer): Constant = {
+    bb.get match {
+      case 0x01 => {
+        val length = bb.getShort
+        val bytes = new Array[Byte](length)
+        bb.get(bytes)
+        Utf8(new String(bytes))
+      }
+      case 0x0C => {
+        NameAndType(bb.getShort, bb.getShort)
+      }
+      case 0x07 => Class(bb.getShort)
+      case 0x08 => ConstString(bb.getShort)
+      case 0x09 => FieldRef(bb.getShort, bb.getShort)
+      case 0x0A => MethodRef(bb.getShort, bb.getShort)
+      case i => throw new Exception(i + " -> " + Span.toHex(i))
+    }
+  }
+}
+
+
+case class Span(start: Int, end: Int, buffer: Array[Byte]) {
+  def readByte: (Int, Span) = {
+    (buffer(0) & 0xFF, skip(1))
+  }
+
+  def readInt: (Int, Span) = (getInt, skip(4))
+
+  def getInt: Int = toByteBuffer.getInt
+
+  def toByteBuffer: ByteBuffer = ByteBuffer.wrap(buffer, start, end - start)
+
+  def skip(i: Int): Span = Span(start + i, end, buffer)
+
+  def toHex: String =
+    buffer.map(Span.toHex).mkString
+
+  def format: String = {
+    import scala.io.AnsiColor.{GREEN, RED, RESET}
+
+    val prefix = toHex.substring(0, start * 2)
+
+    val infix = toHex.substring(start * 2, end)
+
+    val suffix = ""
+
+    GREEN + prefix + RED + infix + GREEN + suffix + RESET
+  }
+
+  def apply(index: Int): Byte = {
+    buffer(start + index)
+  }
+}
+
+
+object Span {
+  def apply(buffer: Array[Byte]): Span = {
+    Span(0, buffer.length, buffer)
+  }
+
+  def toHex(b: Byte): String = {
+    String.format("%02X", Byte.box(b))
+  }
+
+  def toHex(b: Array[Byte]): String =
+    b.map(Span.toHex).mkString
+
+}
+
+object MainApp extends App {
+
+  def read(file: String): ClassFile = {
+    val bytes = Files.readAllBytes(new File(file).toPath)
+
+    ClassFile.parse(bytes)
+  }
+
+  args.headOption.map { file =>
+    println(read(args.head))
+  } getOrElse {
+    println("Please pass file.")
+  }
+}
