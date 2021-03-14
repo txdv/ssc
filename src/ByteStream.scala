d4a946e5e399c0346707eaa7cbe728e0e104aa2e
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-03-14 15:21:14 +0200

    add forgotten bytestream

---
+++ src/ByteStream.scala
@@ -0,0 +1,43 @@
+package lt.vu.mif.bentkus.bachelor.compiler.misc
+
+import java.nio.ByteBuffer
+
+import java.io.{ByteArrayOutputStream => MemoryStream}
+
+trait ByteStream {
+  def putByte(i: Int): Unit
+  def putShort(i: Int): Unit
+  def putInt(i: Int): Unit
+  def putBytes(bytes: Array[Byte]): Unit
+
+  def putString(string: String): Unit = {
+    putBytes(string.getBytes)
+  }
+
+  def getBytes: ByteBuffer
+}
+
+class ByteBufferStream extends ByteStream {
+  val buffer = new Array[Byte](1024 * 1024)
+  val bb = ByteBuffer.wrap(buffer)
+
+  def putByte(i: Int): Unit = {
+    bb.put(i.toByte)
+  }
+
+  def putShort(i: Int): Unit = {
+    bb.putShort(i.toShort)
+  }
+
+  def putInt(i: Int): Unit = {
+    bb.putInt(i)
+  }
+
+  def putBytes(bytes: Array[Byte]): Unit = {
+    bb.put(bytes, 0, bytes.length)
+  }
+
+  def getBytes: ByteBuffer = {
+    ByteBuffer.wrap(buffer, 0, bb.position())
+  }
+}
