f93c0ff92595ef5703f24cc07c9ccb286e309e6a
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-05-04 15:10:56 +0300

    adjust example

--- code/caseclass.scala
+++ code/caseclass.scala
@@ -1,6 +1,4 @@
 sealed trait Expression
-object Expression {
-  case class Int32(value: Int) extends Epression
-  case class Sum(left: Expression, right: Expression) extends Expression
-  case class Multiply(left: Expression, right: Expression) extends Expression
-}
+case class Int32(value: Int) extends Expression
+case class Sum(left: Expression, right: Expression) extends Expression
+case class Multiply(left: Expression, right: Expression) extends Expression
