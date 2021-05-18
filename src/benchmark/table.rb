c32d1a316fc0166eb3a7565898a6c76f612edebb
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-05-18 12:16:48 +0300

    add table

---
+++ src/benchmark/table.rb
@@ -0,0 +1,16 @@
+
+
+require 'json'
+
+#puts JSON.parse(File.read("resi;t)
+puts "Classes & Methods & Prototyp & Cached & Official \\"
+lines = File.read("results.json").lines
+lines.each do |line|
+  result = JSON.parse(line)
+  classes = result["classes"].to_s.rjust(4, ' ')
+  methods = result["methods"].to_s.rjust(3, ' ')
+  new = result["new"]["clock"].to_s.rjust(5, ' ')
+  cache = result["new_cached"]["clock"].to_s.rjust(5, ' ')
+  old = result["old"]["clock"].to_s.rjust(5, ' ')
+  puts "#{classes} & #{methods} & #{new} & #{cache} & #{old} \\"
+end
