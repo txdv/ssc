3ea1ccf42a2f8f6decd1d6db1ccd9cacd75b1522
Andrius Bentkus <andrius.bentkus@mif.stud.vu.lt>
2021-05-18 13:42:58 +0300

    use OpenStruct

--- src/benchmark/table.rb
+++ src/benchmark/table.rb
@@ -1,19 +1,17 @@
-
-
 require 'json'
 
 #puts JSON.parse(File.read("resi;t)
 puts "Classes & Methods & Prototyp & Cached & Diff & Official \\\\"
 lines = File.read("results.json").lines
 lines.each do |line|
-  result = JSON.parse(line)
-  classes = result["classes"].to_s.rjust(4, ' ')
-  methods = result["methods"].to_s.rjust(3, ' ')
-  new = result["new"]["clock"].to_s.rjust(5, ' ')
-  cache = result["new_cached"]["clock"].to_s.rjust(5, ' ')
-  old = result["old"]["clock"].to_s.rjust(5, ' ')
+  obj = JSON.parse(line, object_class: OpenStruct)
+  classes = obj.classes.to_s.rjust(4, ' ')
+  methods = obj.methods.to_s.rjust(3, ' ')
+  new = obj.new.clock.to_s.rjust(5, ' ')
+  cache = obj.new_cached.clock.to_s.rjust(5, ' ')
+  old = obj.old.clock.to_s.rjust(5, ' ')
 
-  diff = (result["new"]["clock"] - result["new_cached"]["clock"]).to_s.rjust(5, ' ')
+  diff = (obj.new.clock - obj.new_cached.clock).to_s.rjust(5, ' ')
 
   puts "#{classes} & #{methods} & #{new} & #{cache} & #{diff} & #{old} \\\\"
 end
